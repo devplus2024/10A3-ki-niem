5efbd67fb88fd9b49850dfd34841d79e5ccc9e09
Phạm Quang Trường An <anpham08112009@gmail.com>
2024-09-05 20:47:30 +0700

    first commit

---
+++ app/opengraph-image.tsx
@@ -0,0 +1,58 @@
+import { ImageResponse } from 'next/og'
+export const runtime = 'edge'
+ 
+// Image metadata
+export const alt = '10A3 Ảnh Kỉ Niệm'
+export const size = {
+  width: 1200,
+  height: 630,
+}
+ 
+export const contentType = 'image/png'
+ 
+// Image generation
+export default async function Image() {
+  // Font
+  const logoSrc = await fetch(new URL('./10A3.png', import.meta.url)).then(
+    (res) => res.arrayBuffer()
+  )
+  const interSemiBold = fetch(
+    new URL('./Inter-SemiBold.ttf', import.meta.url)
+  ).then((res) => res.arrayBuffer())
+ 
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <div
+        style={{
+          fontSize: 128,
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <img src={logoSrc} style={{
+          width: '100%',
+          height: '100%',          
+        }} />
+      </div>
+    ),
+    // ImageResponse options
+    {
+      // For convenience, we can re-use the exported opengraph-image
+      // size config to also set the ImageResponse's width and height.
+      ...size,
+      fonts: [
+        {
+          name: 'Inter',
+          data: await interSemiBold,
+          style: 'normal',
+          weight: 400,
+        },
+      ],
+    }
+  )
+}
