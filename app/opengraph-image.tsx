df40e4a513002b320999faeec42e9dc1673d64c7
Phạm Quang Trường An <anpham08112009@gmail.com>
2024-09-05 20:58:58 +0700

    first commit

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,42 +1,22 @@
 import { ImageResponse } from 'next/og'
-export const runtime = 'edge'
+import { join } from 'node:path'
+import { readFile } from 'node:fs/promises'
  
-// Image metadata
-export const alt = '10A3 Ảnh Kỉ Niệm'
-export const size = {
-  width: 1200,
-  height: 630,
-}
- 
-export const contentType = 'image/png'
- 
-// Image generation
 export default async function Image() {
-  // Font
-  const logoSrc = await fetch(new URL('./10A3.jpg', import.meta.url)).then(
-    (res) => res.arrayBuffer()
-  )
+  const logoData = await readFile(join(process.cwd(), '10A3.png'))
+  const logoSrc = Uint8Array.from(logoData).buffer
+ 
   return new ImageResponse(
     (
-      // ImageResponse JSX element
       <div
         style={{
-          fontSize: 128,
-          background: 'white',
-          width: '100%',
-          height: '100%',
           display: 'flex',
           alignItems: 'center',
           justifyContent: 'center',
         }}
       >
-        <img src={logoSrc} style={{
-          width: '100%',
-          height: '100%',          
-        }} />
+        <img src={logoSrc} height="100" className="max-w-full h-full" />
       </div>
-    ),
-    // ImageResponse options
-    
+    )
   )
 }
